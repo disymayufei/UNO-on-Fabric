13ba1c7fc905762650be065c7a8855543dd78e19
Disy <disymayufei@yeah.net>
2023-12-07 22:56:53 +0800

    chore: 添加部分基础卡枚举

--- src/main/java/cn/disy920/sls_uno/card/Card.java
+++ src/main/java/cn/disy920/sls_uno/card/Card.java
@@ -1,6 +1,7 @@
 package cn.disy920.sls_uno.card;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -30,5 +31,33 @@ public class Card {
                         .forEach(card -> entries.add(card.getItem()))
                 )
                 .build();
+    }
+
+    public enum Basic {
+        RED_0(Color.RED, 0, Items.CARD_0_RED.getItem()),
+        RED_1(Color.RED, 1, Items.CARD_1_RED.getItem());
+
+        // skip的num值为10，reverse为11，+2为12，其余均为-1
+        final int num;
+        final Color color;
+        final Item item;
+
+        Basic(Color color, int num, Item item) {
+            this.color = color;
+            this.num = num;
+            this.item = item;
+        }
+
+        public int getNum() {
+            return num;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public Item getItem() {
+            return item;
+        }
     }
 }
