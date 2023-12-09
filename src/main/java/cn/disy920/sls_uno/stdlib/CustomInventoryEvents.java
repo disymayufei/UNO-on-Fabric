875a2553f079418c2874f66b9b2e31c448c63e44
ZX夏夜之风 <snwcreations@qq.com>
2023-12-09 21:56:52 +0800

    feat: implement custom inventory
    Thank you CraftBukkit

---
+++ src/main/java/cn/disy920/sls_uno/stdlib/CustomInventoryEvents.java
@@ -0,0 +1,63 @@
+package cn.disy920.sls_uno.stdlib;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+/**
+ * Events related to {@link CustomInventory}.
+ * Events under this class only got called in SERVER environment.
+ *
+ * @author SNWCreations
+ */
+public class CustomInventoryEvents {
+
+    /**
+     * Got called when player clicks on our {@link CustomInventory}.
+     */
+    public static final Event<Clicked> CLICK = EventFactory.createArrayBacked(Clicked.class,
+            (player, inv, slot, itemClicked) -> true,
+            listeners -> ((player, inv, slot, itemClicked) -> {
+                boolean noCancel = true;
+                boolean isAir = Items.AIR.equals(itemClicked.getItem());
+                for (Clicked listener : listeners) {
+                    if (isAir && listener.rejectAirClicks()) {
+                        continue;
+                    }
+                    noCancel = noCancel & listener.onClick(player, inv, slot, itemClicked);
+                }
+                return noCancel;
+            }));
+
+    public interface Clicked {
+        /**
+         * @return True if we should NOT cancel the click
+         */
+        boolean onClick(ServerPlayerEntity player, CustomInventory inv, int slot, ItemStack itemClicked);
+
+        /**
+         * Override this and return false if you want to hook into clicks on AIR items!
+         * @return True if we should be called when player clicking on AIR items
+         */
+        default boolean rejectAirClicks() {
+            return true;
+        }
+    }
+
+    /**
+     * Got called when player no longer views the {@link CustomInventory}.
+     */
+    public static final Event<Closing> CLOSING = EventFactory.createArrayBacked(Closing.class,
+            (player, cinv) -> {},
+            listeners -> (player, cinv) -> {
+                for (Closing listener : listeners) {
+                    listener.onClose(player, cinv);
+                }
+            });
+
+    public interface Closing {
+        void onClose(ServerPlayerEntity player, CustomInventory cinv);
+    }
+}
---
+++ src/main/java/cn/disy920/sls_uno/stdlib/mixin/ScreenHandlerMixin.java
@@ -0,0 +1,44 @@
+package cn.disy920.sls_uno.stdlib.mixin;
+
+import cn.disy920.sls_uno.stdlib.CustomInventory;
+import cn.disy920.sls_uno.stdlib.CustomInventoryEvents;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ClickType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ScreenHandler.class)
+public abstract class ScreenHandlerMixin {
+    @Shadow protected abstract boolean handleSlotClick(PlayerEntity player, ClickType clickType, Slot slot, ItemStack stack, ItemStack cursorStack);
+
+    @Redirect(at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/screen/ScreenHandler;handleSlotClick(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/ClickType;Lnet/minecraft/screen/slot/Slot;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z"),
+            method = "internalOnSlotClick")
+    private boolean wrappedHandle(ScreenHandler instance, PlayerEntity player,
+                                  ClickType clickType, Slot slot, ItemStack stack, ItemStack cursorStack) {
+        if (((Object) this) instanceof GenericContainerScreenHandler generic) {
+            if (generic.getInventory() instanceof CustomInventory cinv && slot.inventory == cinv) { // yes our time!
+                if (Items.AIR.equals(stack.getItem())) {
+                    return true; // nop on air items
+                }
+                // we're sure the player is server-sided
+                // because this mixin only available in SERVER environment
+                boolean noCancel = CustomInventoryEvents.CLICK.invoker()
+                        .onClick((ServerPlayerEntity) player, cinv, slot.id, stack);
+                if (!noCancel) {
+                    return true; // already handled
+                }
+            }
+        }
+        // not our time!
+        return handleSlotClick(player, clickType, slot, stack, cursorStack); // should work like vanilla
+    }
+}
