7a9eac477f8c9ca62862f6824d1a454bfe3a55cb
ZX夏夜之风 <snwcreations@qq.com>
2023-12-16 12:59:42 +0800

    fix: items should be registered in the main initializer so that the server can also recognize the items If you don't do this, the server will crash!

--- src/main/java/cn/disy920/sls_uno/Main.java
+++ src/main/java/cn/disy920/sls_uno/Main.java
@@ -1,9 +1,11 @@
 package cn.disy920.sls_uno;
 
+import cn.disy920.sls_uno.card.CardCache;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
+        CardCache.init();
     }
 }
